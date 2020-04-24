212ce917f8e0f9b74e0d924861f0fc1189df8ff1
Nixon <nixon.emoony@gmail.com>
2020-04-24 15:19:38 +0100

    Rewrite to use regex-autoumita

--- examples/eg.rs
+++ examples/eg.rs
@@ -1,6 +1,11 @@
 use pmre::regex;
 
 #[regex]
-const x: i32 = r"^bc(d|e)*$";
+const x: i32 = r"\d{4}-\d{2}-\d{2}";
 
-fn main() {}
+// #[regex]
+// const y: i32 = r"(\d{4})-(\d{2})-(\d{2})";
+
+fn main() {
+    assert!(x.is_match(r"2014-01-01".as_bytes()))
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,41 @@
 #![feature(proc_macro_diagnostic)]
 
 extern crate proc_macro;
-use self::proc_macro::TokenStream;
-use regex_syntax::{hir::Hir as RegexHir, Parser};
+
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Expr, ExprLit, ItemConst, Lit};
+use syn::*;
+
+use regex_automata::{DenseDFA, Regex as RRegex, RegexBuilder};
+
+type Regex = RRegex<DenseDFA<Vec<u32>, u32>>;
 
 #[proc_macro_attribute]
-pub fn regex(_args: TokenStream, tk_in: TokenStream) -> TokenStream {
+pub fn regex(
+    _args: proc_macro::TokenStream,
+    tk_in: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     // Parse the input
     //
     // This has to be done in the main function as the macro can return early if
     // the input token stream isn't a const item, and thus needs to have a
     // return type of TokenStream
     let input = parse_macro_input!(tk_in as ItemConst);
+    // Traverse the AST from syn to capture a regex from the string literal
     let (regex, name) = get_regex(input);
+    // Generate the code and convert it from proc_macro2 to proc_macro
+    proc_macro::TokenStream::from(gen_matcher(regex, name))
+    // proc_macro::TokenStream::from(quote! {
 
-    dbg!(regex);
-    todo!("Bottom");
+    // })
 }
 
-fn get_regex(input: ItemConst) -> (RegexHir, String) {
+fn empty_regex() -> Regex {
+    RegexBuilder::new().build_with_size("").unwrap()
+}
+
+fn get_regex(input: ItemConst) -> (Regex, String) {
     let name = input.ident.to_string();
     // Grab the regex string
     let regex_str = match *input.expr {
@@ -36,9 +51,30 @@ fn get_regex(input: ItemConst) -> (RegexHir, String) {
                 .unwrap()
                 .error("Expected a string literal")
                 .emit();
-            return (RegexHir::empty(), String::new());
+            return (empty_regex(), String::new());
         }
     };
-    let regex = Parser::new().parse(&regex_str).expect("Invalid Regex"); // TODO: better error reporting.
+    let regex = RegexBuilder::new()
+        .build_with_size::<u32>(&regex_str) // TODO: customizability.
+        .expect("Invalid Regex"); // TODO: better error reporting.
+
     (regex, name)
+}
+
+fn gen_matcher(re: Regex, name: String) -> TokenStream {
+    let forward_bytes = re.forward().to_bytes_native_endian().unwrap();
+    let backward_bytes = re.reverse().to_bytes_native_endian().unwrap();
+    let re_name = format_ident!("{}", name);
+    quote! {
+        use lazy_static;
+        use regex_automata;
+        lazy_static::lazy_static!{
+            static ref #re_name: regex_automata::Regex::<regex_automata::DenseDFA<&'static [u32], u32>> = {
+                let fwd_dfa: regex_automata::DenseDFA<_, u32> = unsafe{ regex_automata::DenseDFA::from_bytes( &[#(#forward_bytes),*] ) };
+                let rvs_dfa: regex_automata::DenseDFA<_, u32> = unsafe{ regex_automata::DenseDFA::from_bytes( &[#(#backward_bytes),*]) };
+                regex_automata::Regex::<regex_automata::DenseDFA<&'static [u32], u32>>::from_dfas(fwd_dfa, rvs_dfa)
+            };
+        }
+    }
+    // quote!{}
 }
